2a816b7327588c76dc4d135f66e08433e02d9f45
erkancomez <erkancomez@gmail.com>
2020-12-11 22:44:11 +0300

    initial comment

---
+++ Northwind.DataAccess/Abstract/ProductManager.cs
@@ -0,0 +1,19 @@
+﻿using Northwind.DataAccess;
+using Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Business.Concrete
+{
+    public class ProductManager
+    {
+        public List<Product> GetAll()
+        {
+            ProductDal productDal = new ProductDal();
+            return productDal.GetAll();
+        }
+    }
+}
